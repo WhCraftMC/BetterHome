3d266a2f76bd78033737f4f62749c5e168ac4499
rumble <bilirumble@outlook.com>
2023-11-28 20:46:13 +0800

    feat 初始化项目(2/2)

--- src/main/kotlin/top/whcraft/plugins/BetterHome.kt
+++ src/main/kotlin/top/whcraft/plugins/BetterHome.kt
@@ -4,7 +4,6 @@ import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.info
 
 object BetterHome : Plugin() {
-
     override fun onEnable() {
         info("Successfully running ExamplePlugin!")
     }
